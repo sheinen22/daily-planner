3bc912d4d9b8911aff32ba83844837ea4e98bc17
Stephen Heinen <stephenheinen22@yahoo.com>
2022-08-15 18:29:56 -0500

    added save function and set up local storage, however only the 8:00 section is working

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,21 @@
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
+
+var saveBtn = document.querySelector(".saveBtn");
+function saveInput(event){
+    var input = event.target.attributes[0].value;
+    var input2 = $("." + input).val();
+    localStorage.setItem(input, input2);
+}
+$(".eight").val(localStorage.getItem("eight"))
+$(".nine").val(localStorage.getItem("nine"))
+$(".ten").val(localStorage.getItem("ten"))
+$(".eleven").val(localStorage.getItem("eleven"))
+$(".twelve").val(localStorage.getItem("twelve"))
+$(".one").val(localStorage.getItem("one"))
+$(".two").val(localStorage.getItem("two"))
+$(".three").val(localStorage.getItem("three"))
+$(".four").val(localStorage.getItem("four"))
+$(".five").val(localStorage.getItem("five"))
+
+saveBtn.addEventListener("click", saveInput)
