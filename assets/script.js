7c4fd02c597a91bc6b097f63b754de82bf352309
Stephen Heinen <stephenheinen22@yahoo.com>
2022-08-15 19:35:34 -0500

    added for loop, cant figure out how to appropriately add the class names to the proper places.

--- assets/script.js
+++ assets/script.js
@@ -16,26 +16,42 @@ function saveInput(event){
     var input2 = $("." + input).val();
     localStorage.setItem(input, input2);
 }
-$(".eight").val(localStorage.getItem("eight"))
-$(".nine").val(localStorage.getItem("nine"))
-$(".ten").val(localStorage.getItem("ten"))
-$(".eleven").val(localStorage.getItem("eleven"))
-$(".twelve").val(localStorage.getItem("twelve"))
-$(".one").val(localStorage.getItem("one"))
-$(".two").val(localStorage.getItem("two"))
-$(".three").val(localStorage.getItem("three"))
-$(".four").val(localStorage.getItem("four"))
-$(".five").val(localStorage.getItem("five"))
+$(".eight").val(localStorage.getItem("eight"));
+$(".nine").val(localStorage.getItem("nine"));
+$(".ten").val(localStorage.getItem("ten"));
+$(".eleven").val(localStorage.getItem("eleven"));
+$(".twelve").val(localStorage.getItem("twelve"));
+$(".one").val(localStorage.getItem("one"));
+$(".two").val(localStorage.getItem("two"));
+$(".three").val(localStorage.getItem("three"));
+$(".four").val(localStorage.getItem("four"));
+$(".five").val(localStorage.getItem("five"));
 
-saveBtn0.addEventListener("click", saveInput)
-saveBtn1.addEventListener("click", saveInput)
-saveBtn2.addEventListener("click", saveInput)
-saveBtn3.addEventListener("click", saveInput)
-saveBtn4.addEventListener("click", saveInput)
-saveBtn5.addEventListener("click", saveInput)
-saveBtn6.addEventListener("click", saveInput)
-saveBtn7.addEventListener("click", saveInput)
-saveBtn8.addEventListener("click", saveInput)
-saveBtn9.addEventListener("click", saveInput)
+saveBtn0.addEventListener("click", saveInput);
+saveBtn1.addEventListener("click", saveInput);
+saveBtn2.addEventListener("click", saveInput);
+saveBtn3.addEventListener("click", saveInput);
+saveBtn4.addEventListener("click", saveInput);
+saveBtn5.addEventListener("click", saveInput);
+saveBtn6.addEventListener("click", saveInput);
+saveBtn7.addEventListener("click", saveInput);
+saveBtn8.addEventListener("click", saveInput);
+saveBtn9.addEventListener("click", saveInput);
 
 //for loop for checking the time on the planner vs the current time and use the .addClass function to set the color scheme accordingly.  preset classes are past, future and present
+var plannerHours = document.querySelector(".hour");
+var currentTime = moment().format("H");
+
+for (let i = 0; i < plannerHours.length; i++) {
+    var currentHour = plannerHours[i];
+    var plannerTime = currentHour.outerText;
+    var compareTime = moment(plannerTime, "h").format("h")
+
+    if (currentTime > compareTime) {
+        //add class past
+    } else if (currentTime < compareTime) {
+        //add class future
+    } else {
+        //add class present
+    }
+}
